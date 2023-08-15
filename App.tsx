b8e28f85cf7cd5d0febc7b4c68d99e14d69a3b72
Tommy Linder <tommylinder@Tommys-iMac-2.lan>
2023-08-14 21:14:11 -0500

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { SafeAreaView,Text,View} from 'react-native';
+
+const App = () => {
+    return (
+    <SafeAreaView>
+      <Text style={{
+      color:'#ff0000',
+      fontFamily: 'Arial',
+      fontSize: 50,
+      fontStyle: 'italic',
+      fontWeight: '700',
+      //letterSpacing: 10 ,
+      lineHeight: 50,
+      textAlign: 'center',
+
+
+
+
+
+
+      }}>"Hello World" </Text>
+     </SafeAreaView>
+
+
+    );
+};
+
+
+export default App;
